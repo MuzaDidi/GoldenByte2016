30b46378cd6f6db58437b9a654d5b7b91003c1ca
Diana <didi77777@ukr.net>
2016-06-18 13:18:50 +0300

    Add Scripts

--- GoldenByte2016/Assets/Scripts/InputControllers/MouseController.cs
+++ GoldenByte2016/Assets/Scripts/InputControllers/MouseController.cs
@@ -1,15 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MouseController : MonoBehaviour {
-
+public class MouseController : MonoBehaviour 
+{
+/// <summary>
+/// Provide input from mouse
+/// </summary>
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+    {
+	 //TODO
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+     {
 	
 	}
 }
